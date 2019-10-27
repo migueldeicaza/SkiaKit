f3ef6edbd8f64a6eb917ab68569779298345df6f
miguel <miguel.de.icaza@gmail.com>
2019-10-26 21:07:11 -0400

    Add sample bits

--- Shared/Tests/SKCanvasTest.swift
+++ Shared/Tests/SKCanvasTest.swift
@@ -29,3 +29,28 @@ class SKCanvasTest : XCTestCase {
         canvas.drawText (text: "text", x: 150, y: 175, paint: paint)
     }
 }
+
+//
+//func sampleDrawMatrix (canvas: Canvas, width: Int, height: Int)
+//{
+//    let size = (height > width ? width : height) / 2
+//
+//    let center = Point(x: Float((width-size)/2), y: Float((height-size) / 2))
+//    
+//   //var leftRect = Rect(left: center.x - size / 2, top: center.Y, right: size, bottom: size)
+//   //var rightRect = Rect(left: center.x + size / 2, top: center.Y, right: size, bottom: size
+//   //
+//   //var rotatedRect = Rect(0,0,size,size)
+//   //var paint = Paint()
+//   //paint.isAntialias = true
+//   //canvas.clear(color: Colors.purple)
+//   //paint.color = Colors.darkBlue
+//   //canvas.drawRect(leftRect, paint)
+//    
+//}
+//let bmp = try! Bitmap(ImageInfo (300, 300))
+//let canvas = Canvas(bmp)
+//let paint = Paint()
+//canvas.drawText (text: "text", x: 150, y: 175, paint: paint)
+//
+//print("Hello, Skia!")
