5138f0bf9aeef3ec4b2c464d161efe83c0401287
miguel <miguel.de.icaza@gmail.com>
2019-10-28 22:51:43 -0400

    Small touchups to TextBlob

--- Shared/SkiaKit/TextBlob.swift
+++ Shared/SkiaKit/TextBlob.swift
@@ -21,12 +21,57 @@ public class TextBlob {
         self.handle = handle
     }
     
+    /// Gets the unique, non-zero value representing the text blob.
     public func getUniqueId () -> UInt32
     {
         sk_textblob_get_unique_id(handle)
+    }
+    
+    /// Gets the conservative blob bounding box.
+    public var bounds: Rect {
+        get {
+            var r = sk_rect_t()
+            sk_textblob_get_bounds(handle, &r)
+            return Rect.fromNative(r)
+        }
     }
     
     deinit {
         sk_textblob_unref(handle)
     }
 }
+
+/// A builder object that is used to create a `TextBlob`.
+public class TextBlobBuilder {
+    var handle: OpaquePointer
+    var owns: Bool
+    
+    init (handle: OpaquePointer, owns: Bool)
+    {
+        self.handle = handle
+        self.owns = owns
+    }
+
+    /// Creates a new instance of the TextBlobBuilder
+    public init ()
+    {
+        handle = sk_textblob_builder_new()
+        owns = true
+    }
+    
+    deinit {
+        if owns {
+            sk_textblob_builder_delete(handle)
+        }
+    }
+    
+    /**
+     * Create the SKTextBlob from all the added runs.
+     * - Returns: the new SKTextBlob if there were runs, otherwise null.
+     */
+    public func build () -> TextBlob
+    {
+        TextBlob (handle: sk_textblob_builder_make(handle))
+    }
+}
+
