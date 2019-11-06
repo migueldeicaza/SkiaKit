ffe04fadb358d36d88b251f53d2f5b036e5d1db9
miguel <miguel.de.icaza@gmail.com>
2019-11-05 23:03:22 -0500

    Small text work

--- Shared/SkiaKit/TextBlob.swift
+++ Shared/SkiaKit/TextBlob.swift
@@ -73,5 +73,47 @@ public final class TextBlobBuilder {
     {
         TextBlob (handle: sk_textblob_builder_make(handle))
     }
+    
+    /// Adds a new default-positioned run to the builder.
+    /// - Parameter font: The font to use for this run
+    /// - Parameter x: The x position for this run
+    /// - Parameter y: The y position for this run
+    /// - Parameter glyphs: The glyphs to use for this run
+    public func addRun (font: Paint, x: Float, y: Float, glyphs: [ushort])
+    {
+        let run = allocateRun(font: font, count: Int32 (glyphs.count), x: x, y: y)
+        run.clusters.copyMemory(from: glyphs, byteCount: glyphs.count * 2)
+    }
+    
+    /**
+     * Returns run with storage for glyphs. Caller must write count glyphs to
+     * Glyphs share metrics in font.
+     * Glyphs are positioned on a baseline at (x, y), using font metrics to
+     * determine their relative placement.
+     * bounds defines an optional bounding box, used to suppress drawing when `TextBlob`
+     * bounds does not intersect `Surface` bounds. If bounds is `nil`, `TextBlob` bounds
+     * is computed from (x, y) and RunBuffer::glyphs metrics.
+     * - Parameter font: `Font` used for this run
+     * - Parameter count: number of glyphs
+     * - Parameter x: horizontal offset within the blob
+     * - Parameter y: vertical offset within the blob
+     * - Parameter textByteCount: number of characters that will be provided
+     * - Parameter bounds: optional run bounding box
+     * - Returns: writable glyph buffer
+     */
+    func allocateRun (font: Paint, count: Int32, x: Float, y: Float, textByteCount: Int32 = 0, bounds: Rect? = nil) -> sk_textblob_builder_runbuffer_t
+    {
+        let lang = SKString()
+        var ret = sk_textblob_builder_runbuffer_t ()
+        if let b = bounds {
+            var nb = b.toNative()
+            
+            sk_textblob_builder_alloc_run_text(handle, font.handle, count, x, y, textByteCount, lang.handle, &nb, &ret)
+        } else {
+            sk_textblob_builder_alloc_run_text(handle, font.handle, count, x, y, textByteCount, lang.handle, nil, &ret)
+        }
+        
+        return ret
+    }
 }
 
