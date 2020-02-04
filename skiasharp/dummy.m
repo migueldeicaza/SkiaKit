5379486268d1b405db98e4fd06ffcfad09c68a88
Miguel de Icaza <miguel@gnome.org>
2020-02-04 11:01:07 -0500

    Add missing file

---
+++ skiasharp/dummy.m
@@ -0,0 +1,7 @@
+//
+// This dummy file is here to bring the SkiaKit.h header that has
+// all the definitions
+//
+// The entire module is exported as "CSkiaSharp" - this definition
+// is in include/module.modulemap.   It is not clea
+// #include <SkiaKit.h>
