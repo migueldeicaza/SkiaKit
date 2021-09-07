cca9dfb789cd6599e290e7a35aa90e3d392022f9
Michael Bullington <michael@bullington.xyz>
2021-09-06 22:54:15 -0400

    fix bug with UIImage that crashes my real iOS device

--- Sources/SkiaKit/Apple/SkiaCanvasLayer.swift
+++ Sources/SkiaKit/Apple/SkiaCanvasLayer.swift
@@ -14,6 +14,9 @@ import QuartzCore
 public class SkiaLayer : CALayer {
     public var drawingCallback: (_ surface: Surface, _ imageInfo: ImageInfo) -> ()
     
+    /// Cache bitmap data until next render or deinit.
+    private var oldBitmapData: UnsafeMutableRawPointer? = nil
+
     static func emptyCallback (surface: Surface, imageInfo: ImageInfo)
     {
         // Does nothing
@@ -35,6 +38,12 @@ public class SkiaLayer : CALayer {
         super.init (coder: coder)
     }
     
+    deinit {
+        if oldBitmapData != nil {
+            free (oldBitmapData)
+        }
+    }
+    
     /// If true, this will ignore the pixel scaling of the device, otherwise some virtual pixels might use the number of physical pixels specified in the system
     public var ignorePixelScaling: Bool {
         didSet {
@@ -52,35 +61,38 @@ public class SkiaLayer : CALayer {
             return
         }
         
-        if let bitmapData = malloc(info.bytesSize) {
-            guard let surface = Surface.make (info, bitmapData, info.rowBytes)  else {
-                free (bitmapData)
-                return
-            }
-            drawingCallback (surface, info)
-            surface.canvas.flush ()
-            
-            guard let dataProvider = CGDataProvider(dataInfo: nil, data: bitmapData, size: info.bytesSize, releaseData: {ctx, ptr, size in }) else {
+        if oldBitmapData != nil {
+            free (oldBitmapData)
+        }
+        guard let bitmapData = malloc(info.bytesSize) else {
+            return
+        }
+        // Store so we don't free early.
+        oldBitmapData = bitmapData
 
-                free(bitmapData)
-                return
-            }
-            let colorSpace = CGColorSpaceCreateDeviceRGB()
-            let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue).union(.byteOrder32Little)
-            if let image = CGImage(width: Int(info.width), height: Int(info.height), bitsPerComponent: 8, bitsPerPixel: Int(info.bytesPerPixel*8), bytesPerRow: info.rowBytes, space: colorSpace, bitmapInfo: bitmapInfo, provider: dataProvider, decode: nil, shouldInterpolate: false, intent: .defaultIntent) {
+        guard let surface = Surface.make (info, bitmapData, info.rowBytes)  else {
+            return
+        }
+        drawingCallback (surface, info)
+        surface.canvas.flush ()
+        
+        guard let dataProvider = CGDataProvider(dataInfo: nil, data: bitmapData, size: info.bytesSize, releaseData: {ctx, ptr, size in }) else {
+            return
+        }
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue).union(.byteOrder32Little)
+        if let image = CGImage(width: Int(info.width), height: Int(info.height), bitsPerComponent: 8, bitsPerPixel: Int(info.bytesPerPixel*8), bytesPerRow: info.rowBytes, space: colorSpace, bitmapInfo: bitmapInfo, provider: dataProvider, decode: nil, shouldInterpolate: false, intent: .defaultIntent) {
 #if os(OSX)
-                ctx.draw(image, in: bounds)
+            ctx.draw(image, in: bounds)
 #else
-                // in iOS, WatchOS and tvOS we need to flip the image on
-                // https://developer.apple.com/library/ios/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html#//apple_ref/doc/uid/TP40010156-CH14-SW26
-                ctx.saveGState()
-                ctx.translateBy(x: 0, y: bounds.height)
-                ctx.scaleBy(x: 1, y: -1)
-                ctx.draw(image, in: bounds)
-                ctx.restoreGState()
+            // in iOS, WatchOS and tvOS we need to flip the image on
+            // https://developer.apple.com/library/ios/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html#//apple_ref/doc/uid/TP40010156-CH14-SW26
+            ctx.saveGState()
+            ctx.translateBy(x: 0, y: bounds.height)
+            ctx.scaleBy(x: 1, y: -1)
+            ctx.draw(image, in: bounds)
+            ctx.restoreGState()
 #endif
-            }
-            free (bitmapData)
         }
     }
 }
--- Sources/SkiaKit/Apple/SkiaView.swift
+++ Sources/SkiaKit/Apple/SkiaView.swift
@@ -23,30 +23,33 @@ public class SkiaView: UIView {
     /// This property when set points to the method to invoke when drawing.   The  method
     /// receives a surface and the ImageInfo where it should draw its contents.
     public var drawingCallback: (_ surface: Surface, _ imageInfo: ImageInfo) -> () = emptyCallback(surface:imageInfo:)
-  
-  private lazy var displayLink: CADisplayLink =
-  {
+    
+    /// Cache bitmap data until next render or deinit.
+    private var oldBitmapData: UnsafeMutableRawPointer? = nil
+
+    private lazy var displayLink: CADisplayLink =
+    {
         let link = CADisplayLink(target: self, selector: #selector(onDisplayLink(_:)))
         link.isPaused = true
         return link
-  }()
-  
-  private func commonInit()
-  {
+    }()
+
+    private func commonInit()
+    {
         displayLink.add(to: .main, forMode: .common)
-  }
-    
-  public var loop: Bool = false
-  {
-    didSet {
-      displayLink.isPaused = !loop
     }
-  }
-  
-  @objc private func onDisplayLink(_ sender: CADisplayLink)
-  {
+
+    public var loop: Bool = false
+    {
+    didSet {
+        displayLink.isPaused = !loop
+    }
+    }
+
+    @objc private func onDisplayLink(_ sender: CADisplayLink)
+    {
         setNeedsDisplay()
-  }
+    }
     
     static func emptyCallback (surface: Surface, imageInfo: ImageInfo)
     {
@@ -71,6 +74,12 @@ public class SkiaView: UIView {
         super.init (coder: coder)
         commonInit()
     }
+
+    deinit {
+        if oldBitmapData != nil {
+            free (oldBitmapData)
+        }
+    }
     
     override public func draw(_ rect: CGRect) {
         super.draw (rect)
@@ -82,37 +91,40 @@ public class SkiaView: UIView {
             return
         }
 
-        if let bitmapData = malloc(info.bytesSize) {
-            guard let surface = Surface.make (info, bitmapData, info.rowBytes) else {
-                free (bitmapData)
-                return
-            }
-            drawingCallback (surface, info)
-            surface.canvas.flush ()
-            
-            guard let dataProvider = CGDataProvider(dataInfo: nil, data: bitmapData, size: info.bytesSize, releaseData: {ctx, ptr, size in }) else {
+        if oldBitmapData != nil {
+            free (oldBitmapData)
+        }
+        guard let bitmapData = malloc(info.bytesSize) else {
+            return
+        }
+        // Store so we don't free early.
+        oldBitmapData = bitmapData
 
-                free(bitmapData)
-                return
-            }
-            let colorSpace = CGColorSpaceCreateDeviceRGB()
-            let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue).union(.byteOrder32Little)
-            if let image = CGImage(width: Int(info.width), height: Int(info.height), bitsPerComponent: 8, bitsPerPixel: Int(info.bytesPerPixel*8), bytesPerRow: info.rowBytes, space: colorSpace, bitmapInfo: bitmapInfo, provider: dataProvider, decode: nil, shouldInterpolate: false, intent: .defaultIntent) {
-                if let ctx = UIGraphicsGetCurrentContext() {
+        guard let surface = Surface.make (info, bitmapData, info.rowBytes) else {
+            return
+        }
+        drawingCallback (surface, info)
+        surface.canvas.flush ()
+        
+        guard let dataProvider = CGDataProvider(dataInfo: nil, data: bitmapData, size: info.bytesSize, releaseData: {ctx, ptr, size in }) else {
+            return
+        }
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue).union(.byteOrder32Little)
+        if let image = CGImage(width: Int(info.width), height: Int(info.height), bitsPerComponent: 8, bitsPerPixel: Int(info.bytesPerPixel*8), bytesPerRow: info.rowBytes, space: colorSpace, bitmapInfo: bitmapInfo, provider: dataProvider, decode: nil, shouldInterpolate: false, intent: .defaultIntent) {
+            if let ctx = UIGraphicsGetCurrentContext() {
 #if os(OSX)
-                    ctx.draw(image, in: bounds)
+                ctx.draw(image, in: bounds)
 #else
-                    // in iOS, WatchOS and tvOS we need to flip the image on
-                    // https://developer.apple.com/library/ios/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html#//apple_ref/doc/uid/TP40010156-CH14-SW26
-                    ctx.saveGState()
-                    ctx.translateBy(x: 0, y: bounds.height)
-                    ctx.scaleBy(x: 1, y: -1)
-                    ctx.draw(image, in: bounds)
-                    ctx.restoreGState()
+                // in iOS, WatchOS and tvOS we need to flip the image on
+                // https://developer.apple.com/library/ios/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html#//apple_ref/doc/uid/TP40010156-CH14-SW26
+                ctx.saveGState()
+                ctx.translateBy(x: 0, y: bounds.height)
+                ctx.scaleBy(x: 1, y: -1)
+                ctx.draw(image, in: bounds)
+                ctx.restoreGState()
 #endif
-                }
             }
-            free (bitmapData)
         }
     }
 }
