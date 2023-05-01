04509392c8dc72f36159ddd44b6f58277cae8496
Miguel de Icaza <miguel@gnome.org>
2023-05-01 10:43:44 -0400

    Release version 1.0

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ target = [
 	),
 	.binaryTarget (
 		name: "CSkiaSharpBinary",
-		url: "https://github.com/migueldeicaza/SkiaKit/releases/download/0.9/SkiaSharp.xcframework.zip",
+		url: "https://github.com/migueldeicaza/SkiaKit/releases/download/1.0/SkiaSharp.xcframework.zip",
 		checksum: "be3a2ce1f2587841f643ef2bbc676f33c55dd691b0074ee74c93b4c62e41b506"
 	),
 	.testTarget (
