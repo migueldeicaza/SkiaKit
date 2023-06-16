16264f55b3dc84f8e8541e8d60e696e9b82dd056
Miguel de Icaza <miguel@gnome.org>
2023-06-16 11:10:30 -0400

    Upgrade SkiaKit

--- Package.swift
+++ Package.swift
@@ -36,8 +36,8 @@ target = [
 	),
 	.binaryTarget (
 		name: "CSkiaSharpBinary",
-		url: "https://github.com/migueldeicaza/SkiaKit/releases/download/1.0/SkiaSharp.xcframework.zip",
-		checksum: "be3a2ce1f2587841f643ef2bbc676f33c55dd691b0074ee74c93b4c62e41b506"
+		url: "https://github.com/migueldeicaza/SkiaKit/releases/download/1.2/SkiaSharp.xcframework.zip",
+		checksum: "6aab048910bdd5a24f3321b2476ab6dd5306c1efb5729776d3a780a806b008b1"
 	),
 	.testTarget (
 		name: "SkiaKitTests",
